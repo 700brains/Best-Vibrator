d8e20ebed1fd6123967904b0cb53e4d2bb639f1f
Rafal Kaczor <rafal.kaczor.1993@gmail.com>
2014-10-15 12:14:14 +0200

    1.0.4

--- main.py
+++ main.py
@@ -20,183 +20,199 @@ from jnius import autoclass
 
 # Android-specific actions
 
-if platform=="android":
-	PythonActivity=autoclass("org.renpy.android.PythonActivity")
-	Build=autoclass("android.os.Build$VERSION")
-	ANDROID_VERSION=Build.RELEASE
+if platform == "android":
+    PythonActivity = autoclass("org.renpy.android.PythonActivity")
+    Build = autoclass("android.os.Build$VERSION")
+    ANDROID_VERSION = Build.RELEASE
 
 # Check Kivy required version
 
 kivy.require("1.8.0")
-		
+
+
 class BestVibratorAppVibratorController(object):
+    SERVICE_PORT = 3000
 
-	SERVICE_PORT=3000
-	
-	def __init__(self, **kwargs):
-		super(BestVibratorAppVibratorController, self).__init__(**kwargs)
-		if platform=="android":
-			from plyer import vibrator
-			self.vibrator=vibrator
-			self.check_vibrator()
-		else:
-			self.HAS_VIBRATOR=False
-			
-	def custom_vibrations_time(self, period):
-		if period <=59:
-			return str(int(period))+"s"
-		elif period >=60 and period <=3599:
-			return str(int(period/60))+"min"
-		elif period >=3600:
-			return str(int(period/3600))+"h"
-			
-	def check_vibrator(self):
-		if platform=="android":
-			if not ANDROID_VERSION.startswith("2"):
-				if self.vibrator.exists():
-					return "Your device supports vibrations!"
-				else:
-					return "Your device doesn't support vibrations!"
-			else:
-				return "Your device should support vibrations!"
-		else:
-			return "It is not a mobile device!"
-	
-	def run_vibrator(self, period):
-		path="/run_vibrator"
-		osc.sendMsg(path, [period, ], port=self.SERVICE_PORT)
-			
-	def run_vibrator_schema(self, period, gap):
-		path="/run_vibrator_schema"
-		osc.sendMsg(path, [period, gap, ], port=self.SERVICE_PORT)
-			
-	def stop_vibrator(self):
-		path="/stop_vibrator"
-		stop=1
-		osc.sendMsg(path, [stop, ], port=self.SERVICE_PORT)
-		
+    def __init__(self, **kwargs):
+        super(BestVibratorAppVibratorController, self).__init__(**kwargs)
+        if platform == "android":
+            from plyer import vibrator
+
+            self.vibrator = vibrator
+            self.check_vibrator()
+        else:
+            self.HAS_VIBRATOR = False
+
+    def custom_vibrations_time(self, period):
+        if period <= 59:
+            return str(int(period)) + "s"
+        elif period >= 60 and period <= 3599:
+            return str(int(period / 60)) + "min"
+        elif period >= 3600:
+            return str(int(period / 3600)) + "h"
+
+    def check_vibrator(self):
+        if platform == "android":
+            if not ANDROID_VERSION.startswith("2"):
+                if self.vibrator.exists():
+                    return "Your device supports vibrations!"
+                else:
+                    return "Your device doesn't support vibrations!"
+            else:
+                return "Your device should support vibrations!"
+        else:
+            return "It is not a mobile device!"
+
+    def run_vibrator(self, period):
+        path = "/run_vibrator"
+        osc.sendMsg(path, [period, ], port=self.SERVICE_PORT)
+
+    def run_vibrator_schema(self, period, gap):
+        path = "/run_vibrator_schema"
+        osc.sendMsg(path, [period, gap, ], port=self.SERVICE_PORT)
+
+    def stop_vibrator(self):
+        path = "/stop_vibrator"
+        stop = 1
+        osc.sendMsg(path, [stop, ], port=self.SERVICE_PORT)
+
+
 class BestVibratorAppServiceController(object):
+    def __init__(self, **kwargs):
+        super(BestVibratorAppServiceController, self).__init__(**kwargs)
+        self.SERVICE_ACTIVE = False
+        if platform == "android":
+            from android import AndroidService
 
-	def __init__(self, **kwargs):
-		super(BestVibratorAppServiceController, self).__init__(**kwargs)
-		self.SERVICE_ACTIVE=False
-		if platform=="android":
-			from android import AndroidService
-			self.service=AndroidService("Best Vibrator Service", "running")
-			self.service.start("Best Vibrator Service started")
-			self.SERVICE_ACTIVE=True
-		osc.init()
-	
-	def service_stop(self):
-		self.service.stop()
-		self.SERVICE_ACTIVE=False
-		
+            self.service = AndroidService("Best Vibrator Service", "running")
+            self.service.start("Best Vibrator Service started")
+            self.SERVICE_ACTIVE = True
+        osc.init()
+
+    def service_stop(self):
+        self.service.stop()
+        self.SERVICE_ACTIVE = False
+
+
 class BestVibratorAppWebController(object):
-	
-	def __init__(self, **kwargs):
-		super(BestVibratorAppWebController, self).__init__(**kwargs)
-		import webbrowser
-		self.webbrowser=webbrowser
-		if platform=="android":
-			from plyer import email
-			self.email=email
-	
-	def open_website(self, text):
-		self.webbrowser.open(text)
-	
-	def send_email_to_dev(self):
-		if platform=="android":
-			recipient="rafal.kaczor.1993@gmail.com"
-			subject="BEST VIBRATOR QUESTION"
-			text=""
-			create_chooser=False
-			self.email.send(recipient,subject,text,create_chooser)
+    def __init__(self, **kwargs):
+        super(BestVibratorAppWebController, self).__init__(**kwargs)
+        import webbrowser
+
+        self.webbrowser = webbrowser
+        if platform == "android":
+            from plyer import email
+
+            self.email = email
+
+    def open_website(self, text):
+        self.webbrowser.open(text)
+
+    def send_email_to_dev(self):
+        if platform == "android":
+            recipient = "rafal.kaczor.1993@gmail.com"
+            subject = "BEST VIBRATOR QUESTION"
+            text = ""
+            create_chooser = False
+            self.email.send(recipient, subject, text, create_chooser)
+
 
 class BestVibratorAppAdsController(object):
-	
-	def __init__(self, **kwargs):
-		super(BestVibratorAppAdsController, self).__init__(**kwargs)
-		if platform=="android":
-			import random
-			self.random=random
-			self.AdBuddiz=autoclass("com.purplebrain.adbuddiz.sdk.AdBuddiz")
-			#~ self.AdBuddizLogLevel=autoclass("com.purplebrain.adbuddiz.sdk.AdBuddizLogLevel")
-			self.AdBuddiz.setPublisherKey("82ed36f7-f673-4cb0-b225-2689286f2754")
-			#~ self.AdBuddiz.setTestModeActive()
-			#~ self.AdBuddiz.setLogLevel(self.AdBuddizLogLevel.Error)
-			self.AdBuddiz.cacheAds(PythonActivity.mActivity)
-	
-	def show_ads(self):
-		if platform=="android":
-			tick=self.random.randint(1, 4)
-			if tick==1:
-				self.AdBuddiz.showAd(PythonActivity.mActivity)
-				return True
-			else:
-				return False
-	
-class BestVibratorApp(App):
+    def __init__(self, **kwargs):
+        super(BestVibratorAppAdsController, self).__init__(**kwargs)
+        if platform == "android":
+            import random
 
-	def __init__(self, **kwargs):	
-		super(BestVibratorApp, self).__init__(**kwargs)
-		self.serviceController=BestVibratorAppServiceController()
-		self.vibratorController=BestVibratorAppVibratorController()
-		self.webController=BestVibratorAppWebController()
-		self.adsController=BestVibratorAppAdsController()
-	
-	def on_start(self):
-		use_kivy_settings=False
-		EventLoop.window.bind(on_keyboard=self.hook_keyboard)
-		self.adsController.show_ads()
-		
-	def open_settings(self, *largs):
-		pass
-		
-	def hook_keyboard(self, window, key, *largs):
-		if key in [27, 1001]:
-			if not self.adsController.show_ads():
-				self.close_app()
-			return True
-		return False
-	
-	def on_pause(self):
-		return True
-	
-	def close_app(self):
-		if self.serviceController.SERVICE_ACTIVE:
-			self.vibratorController.stop_vibrator()
-			self.serviceController.service_stop()
-		self.stop()
-	
+            self.random = random
+            self.AdBuddiz = autoclass("com.purplebrain.adbuddiz.sdk.AdBuddiz")
+            # ~ self.AdBuddizLogLevel=autoclass("com.purplebrain.adbuddiz.sdk.AdBuddizLogLevel")
+            self.AdBuddiz.setPublisherKey("82ed36f7-f673-4cb0-b225-2689286f2754")
+            #~ self.AdBuddiz.setTestModeActive()
+            #~ self.AdBuddiz.setLogLevel(self.AdBuddizLogLevel.Error)
+            self.AdBuddiz.cacheAds(PythonActivity.mActivity)
+
+    def show_ads(self):
+        if platform == "android":
+            tick = self.random.randint(1, 4)
+            if tick == 1:
+                self.AdBuddiz.showAd(PythonActivity.mActivity)
+                return True
+            else:
+                return False
+
+
+class BestVibratorApp(App):
+    def __init__(self, **kwargs):
+        super(BestVibratorApp, self).__init__(**kwargs)
+        self.serviceController = BestVibratorAppServiceController()
+        self.vibratorController = BestVibratorAppVibratorController()
+        self.webController = BestVibratorAppWebController()
+        self.adsController = BestVibratorAppAdsController()
+
+    def on_start(self):
+        use_kivy_settings = False
+        EventLoop.window.bind(on_keyboard=self.hook_keyboard)
+        self.adsController.show_ads()
+
+    def open_settings(self, *largs):
+        pass
+
+    def hook_keyboard(self, window, key, *largs):
+        if key in [27, 1001]:
+            if not self.adsController.show_ads():
+                self.close_app()
+            return True
+        return False
+
+    def on_pause(self):
+        return True
+
+    def close_app(self):
+        if self.serviceController.SERVICE_ACTIVE:
+            self.vibratorController.stop_vibrator()
+            self.serviceController.service_stop()
+        self.stop()
+
+
 class BestVibratorAppMain(BoxLayout):
-	pass
+    pass
+
 
 class BestVibratorButton(Button):
-	pass
+    pass
+
 
 class BestVibratorLabel(Label):
-	pass
-	
+    pass
+
+
 class BestVibratorSlider(Slider):
-	pass
+    pass
+
 
 class BestVibratorAppMainView(TabbedPanel):
-	pass
-	
+    pass
+
+
 class BestVibratorAppBottomBar(BoxLayout):
-	pass
-	
+    pass
+
+
 class BestVibratorAppTimeView(BoxLayout):
-	pass
+    pass
+
 
 class BestVibratorAppCustomTimeView(BoxLayout):
-	pass
+    pass
+
 
 class BestVibratorAppSchemaView(BoxLayout):
-	pass
+    pass
+
 
 class BestVibratorAppAboutView(BoxLayout):
-	pass
-	
-if __name__=="__main__":
-	BestVibratorApp().run()
+    pass
+
+
+if __name__ == "__main__":
+    BestVibratorApp().run()
